5e8756cc3594c9920edbddce79d9e319b2a50f6e
Jandir <jandirgregorio@icloud.com>
2026-02-18 14:51:25 -0500

    implemented getShowById to fetch the tv show details

---
+++ src/fetch-helpers.js
@@ -0,0 +1,28 @@
+export const getShowById = async (tvShowId) => {
+  try {
+    const [showResponse, episodesResponse] = await Promise.all([fetch(`https://api.tvmaze.com/shows/${tvShowId}`), fetch(`https://api.tvmaze.com/shows/${tvShowId}/episodes`)]);
+    if (!showResponse.ok) throw Error (`Fetch failed. ${showResponse.status} ${showResponse.statusText}`);
+    if (!episodesResponse.ok) throw Error (`Fetch failed. ${episodesResponse.status} ${episodesResponse.statusText}`);
+
+    const [ showData, episodesData ] = await Promise.all([showResponse.json(),episodesResponse.json()]);
+    // Extract seasons and episodes
+    const numSeasons = episodesData.length;
+    const numEpisodes = episodesData.reduce((total, episode) => total + episode.number, 0);
+
+    // save all data in an object
+    const showObj = {
+      image: showData.image.original,
+      title: showData.name,
+      seasons: numSeasons,
+      episodes: numEpisodes,
+      genres: showData.genres.join(', '),
+      status: showData.status,
+      summary: showData.summary,
+    }
+    return { data: showObj, error: null };
+  }
+  catch (error) {
+    console.warn(`An error occurred: ${error.message}`);
+    return { data: null, error: error};
+  }
+};
