ac09ccc2ad0869783c72a94413aca7088c345a11
Jandir <jandirgregorio@icloud.com>
2026-02-18 15:36:11 -0500

    added event listeners in main.js

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import { renderSingleShowDetails } from './dom-helpers';
+import { getShowById } from './fetch-helpers';
+
+const showsList = document.querySelector('ul');
+const closeButton = document.querySelector('#show-close-details');
+const showDetails = document.querySelector('#show-details');
+
+showsList.addEventListener('click', (event) => {
+  const clickedLi = event.target.closest('li');
+  if (!clickedLi) return;
+
+  const tvShowId = clickedLi.dataset.id;
+
+  const { data, error } = getShowById(tvShowId);
+  
+  if (error) {
+    return;
+  }
+  renderSingleShowDetails(data);
+});
+
+// Hide modal/details when user clicks on the button
+closeButton.addEventListener('click', () => {
+  showDetails.classList.add('hidden');
+});
