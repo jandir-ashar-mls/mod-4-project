d7a03cb448306769bf07212b086240f09606b445
Jandir <jandirgregorio@icloud.com>
2026-02-23 15:43:37 -0500

    handled autofocus and scroll behavior for modal in css and added event listener to close modal when user cliks out of screen

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { getShowById } from './fetch-helpers';
 const showsList = document.querySelector('ul');
 const closeButton = document.querySelector('#show-close-details');
 const showDetails = document.querySelector('#show-details');
+const showContent = document.querySelector('#show-content');
 
 showsList.addEventListener('click', async (event) => {
   const clickedLi = event.target.closest('li');
@@ -12,7 +13,7 @@ showsList.addEventListener('click', async (event) => {
   const tvShowId = clickedLi.dataset.id;
 
   const { data, error } = await getShowById(tvShowId);
-  
+
   if (error) {
     return;
   }
@@ -24,4 +25,8 @@ closeButton.addEventListener('click', () => {
   showDetails.close();
 });
 
-showDetails.showModal();
+showDetails.addEventListener('click', (event) => {
+  if(!showContent.contains(event.target)) {
+    showDetails.close();
+  }
+});
